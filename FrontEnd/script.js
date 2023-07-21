6bbb1584c73c29776a6d5169a4cf0f67946492bf
Maeva95 <maeva.tarati@gmail.com>
2023-07-21 14:39:21 +0200

    pafge connexion et autho user

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,8 +1,29 @@
+
 const urlWorks = 'http://localhost:5678/api/works';
 const urlCategories = 'http://localhost:5678/api/categories';
+const urlUsersLogin = 'http://localhost:5678/api/users/login';
+
+//Récupération des pièces eventuellement stockées dans le localStorage
+let userId = window.localStorage.getItem('userId');
+let userToken = window.localStorage.getItem('token');
+let loggedIn = userId && userToken ;
+
+
+// vérifier si le token est présent ds le LS
+if (userId && userToken === null){
+    fetch(urlUsersLogin)
+    .then((response) => response.json())
+    .then((userData) => {
+        userData = { userId, token };
+        window.localStorage.setItem("userData", userData)
+    })
+}
+
+
 
 // Affichage de tous les projets dans la galerie //
-function getWorks(data) {
+function displayWorks(data) {
+    document.querySelector(".gallery").innerHTML = "";
     for (const work of data) {
         const worksGallery = document.querySelector(".gallery");
         const workElement = document.createElement("figure");
@@ -21,7 +42,7 @@ function getWorks(data) {
 // Appel de l'API http://localhost:5678/api/works
 fetch(urlWorks)
 .then((response) => response.json())
-.then((data) => getWorks(data))
+.then((data) => displayWorks(data))
 .catch((error) => {console.log(`Une erreur est survenue : ${error.message}`)})
 
 
@@ -60,28 +81,18 @@ function filtersWorks (works) {
         const filterButton = document.querySelector(`.categories button:nth-child(${i+1})`);
         // ajout d'eventListener pour chacun des boutons
         filterButton.addEventListener("click", () => {
-
+            
             //activer ou désactiver les boutons de filtre
             let filterButtonSelect = document.querySelector(".category-btn-selected");
             filterButtonSelect.classList.remove("category-btn-selected");
             filterButton.classList.add("category-btn-selected");
-            document.querySelector(".gallery").innerHTML = "";
             
             // création des projets filtrés par catégorie
             let worksFiltered = works.filter((work) => {
-                return work.categoryId === i;
+                return work.categoryId === i || i === 0;
             });
-            console.table(worksFiltered)
-            //le bouton "tous" retourne tous les projets
-            if (i === 0) {
-                return getWorks(works);
-            }
-            //les data-id des boutons doivent correspondre aux data-id des projets 
-            //et retourner les projets filtrés
-            else {
             //afficher les projets filtrés
-                return getWorks(worksFiltered)
-            }
+                return displayWorks(worksFiltered)
         });
     }     
 }
@@ -90,3 +101,52 @@ fetch(urlWorks)
 .then((resp) => resp.json())
 .then((works) => filtersWorks (works))
 .catch((error) => {console.log(`Une erreur est survenue : ${error.message}`)})
+
+// ajout homepage edit
+
+function createModeEdition() {
+    //modif texte lien nav "login" en "logout"
+    document.querySelector("nav li:nth-child(3) a").innerText = "logout";
+
+    const header = document.querySelector("header");
+    let headerEdition = 
+        `<ul class="modeEdition">
+            <li><i class="fa-regular fa-pen-to-square"></i></li>
+            <li>Mode édition</li>
+            <li><a class="publicationChange">publier les changements</a></li>
+        </ul>`
+    header.insertAdjacentHTML("afterbegin", headerEdition);
+    // ajout bouton modif intro
+    const introductionImage = document.querySelector(".introductionImage");
+    const introductionIconEdit = document.createElement("i");
+    introductionIconEdit.className = "fa-regular fa-pen-to-square";
+    const introductionBtnEdit = document.createElement("a");
+    introductionBtnEdit.innerText = "modifier";
+    introductionImage.appendChild(introductionIconEdit);
+    introductionImage.appendChild(introductionBtnEdit);
+    // ajout bouton modif portfolio
+    const portfolioSection = document.querySelector("#portfolio");
+    const portfolioCatgories = document.querySelector(".categories")
+    const portfolioDivEdit = document.createElement("div");
+    portfolioDivEdit.className = "edition-gallery";
+    const portfolioIconEdit = document.createElement("i");
+    portfolioIconEdit.className = "fa-regular fa-pen-to-square";
+    const portfolioBtnEdit = document.createElement("a");
+    portfolioBtnEdit.innerText = "modifier";
+
+    /*const iconeEdit = document.createElement("i");*/
+    portfolioSection.appendChild(portfolioDivEdit);
+    portfolioDivEdit.appendChild(portfolioIconEdit);
+    portfolioDivEdit.appendChild(portfolioBtnEdit);
+    portfolioSection.insertBefore(portfolioDivEdit, portfolioCatgories);
+
+    /*
+    iconeEdit.className = "fa-regular fa-pen-to-square";
+        portfolioEditBtn.appendChild(iconeEdit);
+    */
+
+
+    
+}
+
+createModeEdition()
